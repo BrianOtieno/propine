45bff9bb6cbd93b238ddb9f7dd4e4464d4e788b6
BrianOtieno <gebryo@intelligencia.com>
2022-07-16 17:50:47 +0300

    add validation helper

---
+++ propine/validationHelper.js
@@ -0,0 +1,11 @@
+import moment from 'moment'
+import chalk from 'chalk';
+
+export const isValidDate = (date) => {
+    if (moment(date).isValid()) {
+        return true;
+    } else {
+        console.log(chalk.hex('#DEADED').bold(date + ' is an invalid Date'));
+        process.exit();
+    }
+}

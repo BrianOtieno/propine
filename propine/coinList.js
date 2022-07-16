3b6096d7a6708917989e643d568f7a9caa5e7409
BrianOtieno <gebryo@intelligencia.com>
2022-07-16 14:49:20 +0300

    add api key

--- propine/coinList.js
+++ propine/coinList.js
@@ -1,7 +1,8 @@
 import fetch from "node-fetch";
 
 export default () => {
-    const url = "https://min-api.cryptocompare.com/data/blockchain/list";
+    const API_KEY = "4aa7b33d99d9d58782455f766ea79b2c3fce2d566167575c0bf726d319526f68";
+    const url = `https://min-api.cryptocompare.com/data/blockchain/list` + `&api_key=` + API_KEY;
 
     fetch(url).then(async (response) => {
         const data = await response.json();
